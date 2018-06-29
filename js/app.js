da6b989ae7ba16f5fda4298254cc863ae2dc6be6
domani samuel <domanisamuel@gmail.com>
2018-06-29 11:26:50 +0300

    modified styles, views and fetched the api in JSON format

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+//get the url
+const url = 'https://free.currencyconverterapi.com/api/v5/currencies'
 
-const loadCurrencies =()=> {
-    var from = document.getElementById('from');
-    var to = document.getElementById('to');
-
-
-}
+// Call the fetch function passing the url of the API as a parameter and Transform the data into json
+fetch(url) .then(response => {
+    return response.json();
+}).then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log("sorry....error", err)
+});
