d52ff77248534c16403bfc86577bdf82cf8703bf
domani samuel <domanisamuel@gmail.com>
2018-06-29 12:48:48 +0300

    application working online

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Currency-Converter
 Currency Converter App
+
+>Author: Domani Samuel
+>
+>------------------------------------
+>
+### ALC - 7 days of code challange
+>
+>Project Name: Currency Converter
+>
+>-------------------------------------
+>
+### To run the application locally
+* You can use "LIVE SERVER" extension in VScode editor if you happen to use VScode editor.
+>
+>---------------------------
+>
+>Link to deployed page _(click here)_<a href=https://domanisamuel.github.io/Currency-Converter/ title="Title">
+Currency Converter</a>
+>
+>---------------------------
+>
+### licenses and terms of use  
+this application's source code is free for any open source projects
+>
+>---------------------------
+>
+&copy; domanisamuel | Credits <a href="https://andela.com/alcwithgoogle/" title="Title">ALC with Google.</a>
--- js/app.js
+++ js/app.js
@@ -5,7 +5,61 @@ const url = 'https://free.currencyconverterapi.com/api/v5/currencies'
 fetch(url) .then(response => {
     return response.json();
 }).then(data => {
-    console.log(data);
-}).catch(err => {
+    // console.log(data);
+
+    const currencyArray = Object.entries(data.results);
+    let testMap = new Map();
+
+    for(const currency of currencyArray) {
+        let currencyName = currency[1].currencyName;
+        let currencyId = currency[1].id;
+
+        testMap.set(currency[1].id, currency[1].currencyName);
+    }
+    return testMap;
+
+})
+.then(currencyMap => {
+    let select_from_currency = document.getElementById('from_currency');
+    let select_to_currency = document.getElementById('to_currency');
+
+    for(const curr of currencyMap) {
+        let[id, name] = curr;
+
+        select_from_currency.add(new Option(name, id));
+        select_to_currency.add(new Option(name, id));
+    }
+
+})
+.catch(err => {
     console.log("sorry....error", err)
+});
+
+//Conversion begins here lol...
+const form = document.getElementById('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let from_Amount = document.getElementById('from_currency').value;
+    let to_Amount = document.getElementById('to_currency').value;
+
+    let queryUrl = 'https://free.currencyconverterapi.com/api/v5/convert?q='
+    let queryString = queryUrl + from_Amount + '_' + to_Amount;
+
+    fetch(queryString)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const queryResults = Object.entries(data.results);
+        let rate = queryResults[0][1].val;
+        return rate;
+    })
+    .then(rate => {
+        let from_value = document.getElementById('from_amount').value;
+        let converted_value = rate * from_value;
+        document.getElementById('to_amount').value = converted_value;
+
+    })
 });
